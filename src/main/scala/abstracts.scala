2e5aae5b7c6c562f8eb8096dabf750b00a8849ab
John O'Sullivan <josullivan1@luc.edu>
2018-05-13 11:09:15 -0500

    Added grammer

--- src/main/scala/abstracts.scala
+++ src/main/scala/abstracts.scala
@@ -7,25 +7,50 @@ import matryoshka.data.Fix
 //abstract syntax tree
 object abstracts {
 
-    sealed trait ExprF[+A]
+  sealed trait ExprF[+A]
 
-    case class Constant(value: Int) extends ExprF[Nothing]
-    case class Variable(name: String) extends ExprF[Nothing]
-    case class Block[A](expressions: List[A]) extends ExprF[A]
+  case class Function() extends ExprF[Nothing]
 
-    implicit object exprFFunctor extends Functor[ExprF] {
-      def map[A, B](fa: ExprF[A])(f: A => B): ExprF[B] = fa match {
-        case e @ Constant(v) => e
-        case e @ Variable(n) => e
-        case Block(es) => Block(es map f)
-      }
-    }
+  case class Constant(value: Int) extends ExprF[Nothing]
+  case class Variable(name: String) extends ExprF[Nothing]
 
-    type Expr = Fix[ExprF]
+  case class UMinus[A](expr: A) extends ExprF[A]
+  case class Plus[A](left: A, right: A) extends ExprF[A]
+  case class Minus[A](left: A, right: A) extends ExprF[A]
+  case class Times[A](left: A, right: A) extends ExprF[A]
+  case class Div[A](left: A, right: A) extends ExprF[A]
+  case class Mod[A](left: A, right: A) extends ExprF[A]
 
-    object factory {
-      def constant(c: Int) = Fix[ExprF](Constant(c))
-      def variable(n: String) = Fix[ExprF](Variable(n))
-      def block(es: Expr*) = Fix[ExprF](Block(es.toList))
-    }
+  case class Assignment[A](left: List[String], right: A) extends ExprF[A]
+  case class Cond[A](guard: A, thenBranch: A, elseBranch: A) extends ExprF[A]
+  case class Loop[A](guard: A, body: A) extends ExprF[A]
+  case class Block[A](expressions: List[A]) extends ExprF[A]
+
+  case class Func[A](args: List[String], body: A) extends ExprF[A]
+  case class Apply[A](func: A, args: List[A]) extends ExprF[A]
+  case class Struct[A](fields: Map[String, A]) extends ExprF[A]
+  case class Select[A](receiver: A, field: String) extends ExprF[A]
+
+  type Expr = Fix[ExprF]
+
+  object factory {
+    def constant(c: Int) = Fix[ExprF](Constant(c))
+    def function() = Fix[ExprF](Function())
+    def variable(n: String) = Fix[ExprF](Variable(n))
+    def uminus(r: Expr) = Fix[ExprF](UMinus(r))
+    def plus(l: Expr, r: Expr) = Fix[ExprF](Plus(l, r))
+    def minus(l: Expr, r: Expr) = Fix[ExprF](Minus(l, r))
+    def times(l: Expr, r: Expr) = Fix[ExprF](Times(l, r))
+    def div(l: Expr, r: Expr) = Fix[ExprF](Div(l, r))
+    def mod(l: Expr, r: Expr) = Fix[ExprF](Mod(l, r))
+    def assignment(l: List[String], r: Expr) = Fix[ExprF](Assignment(l, r))
+    def cond(g: Expr, t: Expr, e: Expr) = Fix[ExprF](Cond(g, t, e))
+    def loop(g: Expr, b: Expr) = Fix[ExprF](Loop(g, b))
+    def block(es: Expr*) = Fix[ExprF](Block(es.toList))
+    def func(as: List[String], b: Expr) = Fix[ExprF](Func(as, b))
+    def apply(f: Expr, xs: Expr*) = Fix[ExprF](Apply(f, xs.toList))
+    def struct(fs: (String, Expr)*) = Fix[ExprF](Struct(fs.toMap))
+    def select(r: Expr, n: String) = Fix[ExprF](Select(r, n))
+  }
+  
 }
--- src/main/scala/parser.scala
+++ src/main/scala/parser.scala
@@ -3,18 +3,88 @@ package replfun
 import org.parboiled2._
 import abstracts.Expr
 import abstracts.factory._
+import CharPredicate._
 
 class parser(val input: ParserInput) extends Parser {
 
   def Input = rule { zeroOrMore(' ') ~ zeroOrMore(Statement) ~ EOI ~> (block((_: Seq[Expr]): _*)) }
 
   def Statement = rule {
-    Number | Variable
+    Expression ~ ws(';') | Assignment | Conditional | Loop | Block
+  }
+
+  def Assignment = rule {
+    capture(Ident) ~ zeroOrMore(WhiteSpace ~ ws(".") ~ capture(Ident)) ~ WhiteSpace ~ ws('=') ~ Expression ~ ws(';') ~> ((i: String, is: Seq[String], e: Expr) => assignment(i +: is.toList, e))
+  }
+
+  def Conditional: Rule1[Expr] = rule {
+    ws("if") ~ ws('(') ~ Expression ~ ws(')') ~
+      Block ~
+      zeroOrMore(ElseIf) ~
+      optional(ws("else") ~ Block) ~>
+      ((c0: Expr, b0: Expr, es: Seq[(Expr, Expr)], e: Option[Expr]) => {
+        val bn1 = e getOrElse (block())
+        val e0 = es.lastOption map {
+          case (cn, bn) =>
+            es.init.foldRight(cond(cn, bn, bn1))((e, r) => cond(e._1, e._2, r))
+        } getOrElse bn1
+        cond(c0, b0, e0)
+      })
+  }
+
+  def ElseIf: Rule1[(Expr, Expr)] = rule {
+    ws("else") ~ ws("if") ~ ws('(') ~ Expression ~ ws(')') ~ Block ~>
+      ((_: Expr, _: Expr))
+  }
+
+  def Loop = rule {
+    ws("while") ~ ws('(') ~ Expression ~ ws(')') ~ Block ~>
+      (loop(_: Expr, _: Expr))
+  }
+
+  def Block: Rule1[Expr] = rule {
+    ws('{') ~ zeroOrMore(Statement) ~ ws('}') ~> (block((_: Seq[Expr]): _*))
+  }
+
+  def Expression = rule {
+    Term ~ zeroOrMore(
+      ws('+') ~ Term ~> (plus(_: Expr, _))
+        | ws('-') ~ Term ~> (minus(_: Expr, _)))
+  }
+
+  def Term = rule {
+    Factor ~ zeroOrMore(
+      ws('*') ~ Factor ~> (times(_: Expr, _))
+        | ws('/') ~ Factor ~> (div(_: Expr, _))
+        | ws('%') ~ Factor ~> (mod(_: Expr, _)))
+  }
+
+  def Factor: Rule1[Expr] = rule {
+    Selection | Number | UnaryPlus | UnaryMinus | Parens | Struct
+  }
+
+  def Selection: Rule1[Expr] = rule {
+    Variable ~ zeroOrMore(ws('.') ~ capture(Ident)) ~ WhiteSpace ~>
+      ((e: Expr, fs: Seq[String]) => fs.foldLeft(e)(select))
+  }
+
+  def Struct = rule {
+    ws('{') ~ ws('}') ~> (() => struct()) |
+      ws('{') ~ Field ~ zeroOrMore(ws(',') ~ Field) ~ ws('}') ~>
+      ((f: (String, Expr), fs: Seq[(String, Expr)]) => struct((f +: fs): _*))
+  }
+
+  def Field = rule {
+    capture(Ident) ~ WhiteSpace ~ ws(':') ~ Expression ~> ((_: String, _: Expr))
   }
 
   def Number = rule { capture(Digits) ~ WhiteSpace ~> ((s: String) => constant(s.toInt)) }
 
-  import CharPredicate._
+  def UnaryPlus = rule { ws('+') ~ Factor }
+
+  def UnaryMinus = rule { ws('-') ~ Factor ~> (uminus(_: Expr)) }
+
+  def Parens = rule { ws('(') ~ Expression ~ ws(')') ~ WhiteSpace }
 
   def Digits = rule { oneOrMore(Digit) }
 
